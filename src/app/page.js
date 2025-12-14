547adc059db80c40678b4bcf18e639a35f91998a
Brooke Zrymiak <brookezrymiak@Brooke-2.local>
2025-12-14 01:01:12 -0800

    testing vercel

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,18 @@ import Button from "@/components/button.js";
 
 export default function Home() {
   return (
-    <div className="px py">
+    <div>
+      
       
 
-      <Button title={"testing"} cta={"https://stardewvalleywiki.com/Stardew_Valley_Wiki"}></Button>
-
-      <section>
+      <section className="px py">
         <h1 className="mt-8">Recipe Book</h1>
         <h2>(digitized)</h2>
+      </section>
+
+      <section className="px py">
+        <Button title={"view all"} cta={"https://stardewvalleywiki.com/Stardew_Valley_Wiki"}></Button>
+
       </section>
     </div>
   );
